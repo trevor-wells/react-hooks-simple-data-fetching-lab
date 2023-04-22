ac87d08de002a21b4500d854fa6d079ede928ff1
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-21 20:33:59 -0600

    this literally works but i'm failing the tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, {useEffect, useState} from "react"
+
+export default function App(){
+
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("http://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    return(
+        image ? <img src={image} alt="A Random Dog"/> : <p>Loading...</p>
+    )
+}
